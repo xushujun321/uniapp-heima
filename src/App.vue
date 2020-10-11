08be2375b7e1e54f5e81b2943c39b209fb244752
xsj321 <1932806752@qq.com>
2020-10-11 11:15:47 +0800

    设置公共样式

--- src/App.vue
+++ src/App.vue
@@ -14,5 +14,18 @@
 
 <style>
 	/*每个页面公共css */
-	@import "./styles/iconfont.wxss";
+	@import "@/styles/iconfont.wxss";
+
+	 page,view,text,navigator,swiper,swiper-item,image{
+        box-sizing: border-box;
+    }
+    image{
+        width: 100%;
+        display: block;
+    }
+    view{
+        /* 设计稿的宽度 375px 1px = 2rpx */
+        font-size: 28rpx;
+        color:#666;
+    }
 </style>
